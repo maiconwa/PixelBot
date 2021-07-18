a72f80aceef5805bc0161ad30b6e99836b2e2d51
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-07-18 20:01:19 -0300

    PixelBot
    Publicação inicial do PixelBot.

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+from random import randint
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Usuário {0.user} logado'.format(client))
+
+
+@client.event
+async def on_message(mensagem):
+    if mensagem.author == client.user:
+        return
+
+    if mensagem.content.startswith('!Bot'):
+        await mensagem.channel.send('PixelBot aqui')
+
+    if mensagem.content.startswith('!D20'):
+        await mensagem.channel.send(randint(1, 20))
+
+client.run('Insira aqui o seu TOKEN do bot do discord!')
